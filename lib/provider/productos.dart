338d1f1553a0404ce4e01dffcc6d658d3c6450ec
GermanCCC <germancaraveocastillo@gmail.com>
2025-12-30 10:39:36 -0700

    WIP: Se crean métodos para agregar y eliminar productos y se crea variable listado de productos

--- lib/provider/productos.dart
+++ lib/provider/productos.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Productos extends ChangeNotifier {}
+class Productos extends ChangeNotifier {
+  final List<String> _productos = [];
+
+  List<String> get productos => List.unmodifiable(_productos);
+
+  void agregarProducto(String producto) {
+    _productos.add(producto);
+    notifyListeners();
+  }
+
+  void eliminarProducto(String producto) {
+    _productos.remove(producto);
+    notifyListeners();
+  }
+}
